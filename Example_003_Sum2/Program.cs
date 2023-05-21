b9af4cd2059a03c7d14e55e0a74ff9487da94186
Aleksandr_B <alexanderbrudanin@gmail.com>
2023-05-21 22:39:33 +0300

    Упражнение 3 на сложение 2ух чисел

---
+++ Example_003_Sum2/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = 32;
+int numberB = 57;
+//решение без переменной result - Console.Write(numberA + numberB);
+int result = numberA + numberB;
+Console.WriteLine(result); // Решение с переменной result
+

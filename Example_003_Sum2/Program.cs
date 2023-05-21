88888dd7c874d270eda758d3ebff1dd08a9315f9
Aleksandr_B <alexanderbrudanin@gmail.com>
2023-05-21 23:08:45 +0300

    Добавили рандом

--- Example_003_Sum2/Program.cs
+++ Example_003_Sum2/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 32;
-int numberB = 57;
+﻿int numberA = new Random().Next(1, 10);//1 2 3 4 5 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 //решение без переменной result - Console.Write(numberA + numberB);
 int result = numberA + numberB;
 Console.WriteLine(result); // Решение с переменной result

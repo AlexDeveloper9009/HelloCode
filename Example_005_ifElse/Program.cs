70bdc5280a6ed6d072e283c4af8335f92363c296
Aleksandr_B <alexanderbrudanin@gmail.com>
2023-05-21 23:25:31 +0300

    Операторы Ветвления

---
+++ Example_005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя Пользователя!");
+string userName = Console.ReadLine();
+
+if (userName.ToLower == "cаша")
+{
+Console.WriteLine("Ура , Санёчек пришел!");
+
+}
+else 
+{
+    Console.WriteLine("Привет, " + userName);
+}
